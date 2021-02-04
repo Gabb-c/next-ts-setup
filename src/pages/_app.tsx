48d1819ec9260c45bf8f0315d60fc06bc31db8fc
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-02-04 17:45:46 -0300

    Fix Eslint bug import .ts files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import GlobalStyle from '../styles/global';
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <GlobalStyle />
+  </>
 );
 
 export default MyApp;
