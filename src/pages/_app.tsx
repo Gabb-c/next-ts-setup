f3d1ff409fd43b0ff44894400cbb029c4e1433b2
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-02-04 14:31:44 -0300

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+    </Head>
+
+    <main>
+      <h1>Hello world</h1>
+    </main>
+  </div>
+);
+
+export default Home;
