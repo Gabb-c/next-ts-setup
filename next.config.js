d2bb4d41b65ce216d2ec8f7350b6027f333fbda2
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-04-19 00:17:45 -0300

    Add Webpack 5

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 const withImages = require('next-images');
 const withBundleAnalyzer = require('@next/bundle-analyzer')
 
+// Enable inline images
 module.exports = withImages({
   esModule: true,
   fileExtensions: ['jpg', 'jpeg', 'png', 'svg', 'gif', 'ico', 'webp', 'jp2', 'avif']
 });
 
+// Bundle analyzer plugin
 module.exports = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
+
+// Enable Webpack 5
+module.exports = {
+  future: {
+    webpack5: true
+  }
+}
