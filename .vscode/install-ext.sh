d80d5eee975ddb9c43b9c70e881312ea1249c2f7
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-04-27 12:18:15 -0300

    chore: update vscode settings

--- .vscode/install-ext.sh
+++ .vscode/install-ext.sh
@@ -6,10 +6,11 @@ code --install-extension dbaeumer.vscode-eslint &&
 code --install-extension eamodio.gitlens &&
 code --install-extension ecmel.vscode-html-css &&
 code --install-extension EditorConfig.EditorConfig &&
-code --install-extension eg2.vscode-npm-script &&
+code --install-extension jpoissonnier.vscode-styled-components &&
 code --install-extension mikestead.dotenv &&
-code --install-extension ms-azuretools.vscode-docker &&
 code --install-extension ms-vscode.cpptools &&
+code --install-extension ms-vscode.vscode-typescript-next &&
 code --install-extension redhat.vscode-yaml &&
+code --install-extension VisualStudioExptTeam.vscodeintellicode &&
 code --install-extension xabikos.JavaScriptSnippets &&
 code --install-extension Zignd.html-css-class-completion
